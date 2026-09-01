3c53fbb8606d1c9d7ef27daddb9d0df0f435f509
MrDino <cebriancarloscc@gmail.com>
2026-09-02 01:01:02 +0200

    Enforce light occlusion through opaque blocks

--- src/test/java/es/mrdino/strobelights/resourcepack/ShaderPackContractTest.java
+++ src/test/java/es/mrdino/strobelights/resourcepack/ShaderPackContractTest.java
@@ -470,6 +470,20 @@ class ShaderPackContractTest {
         assertContains(config, "scene-view-range: 128.0");
     }
 
+    @Test
+    void makesOpaqueBlockOcclusionMandatoryForEveryRgbEffect() throws IOException {
+        Path manager = Path.of(
+            "src/main/java/es/mrdino/strobelights/service/StrobeManager.java"
+        );
+        Path config = Path.of("src/main/resources/config.yml");
+        assertContains(manager, "sourceBlockedForPlayer(player, source)");
+        assertContains(manager, "sourceBlockedForPlayer(player, scene.location)");
+        assertContains(manager, "if (blockedByGeometry(eye, direction, distance))");
+        assertContains(manager, "blocksLight(world.getBlockAt(blockX, blockY, blockZ).getType())");
+        assertNotContains(manager, "requireLineOfSight");
+        assertNotContains(config, "require-line-of-sight");
+    }
+
     private static void assertContains(Path file, String expected) throws IOException {
         assertTrue(read(file).contains(expected), () -> file + " no contiene " + expected);
     }
