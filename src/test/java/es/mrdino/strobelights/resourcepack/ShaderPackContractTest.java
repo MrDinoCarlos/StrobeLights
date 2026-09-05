61a22d3d4230504be091cf7a18c3c6dd1c8d220c
MrDino <cebriancarloscc@gmail.com>
2026-09-06 01:01:04 +0200

    Fix Nexo merge and resource pack URL recovery

--- src/test/java/es/mrdino/strobelights/resourcepack/ShaderPackContractTest.java
+++ src/test/java/es/mrdino/strobelights/resourcepack/ShaderPackContractTest.java
@@ -591,7 +591,7 @@ class ShaderPackContractTest {
         assertContains(config, "flight-light-expansion: 2.0");
         assertContains(config, "maximum-duration-ticks: 50");
         assertContains(config, "strength-percent: 85");
-        assertContains(config, "config-version: 6");
+        assertContains(config, "config-version: 7");
         assertContains(config, "ground-projection:");
         assertContains(config, "maximum-drop-distance: 128.0");
         assertContains(config, "scene-view-range: 192.0");
